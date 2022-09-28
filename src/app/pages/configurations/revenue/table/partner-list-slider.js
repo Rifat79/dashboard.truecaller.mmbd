edec9d3d0badebce495a39dc36b0eb0676ec0660
Rifat-MMBD <rifat@momagicbd.com>
2022-09-28 13:03:31 +0600

    fixing issues

--- src/app/pages/configurations/revenue/table/partner-list-slider.js
+++ src/app/pages/configurations/revenue/table/partner-list-slider.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Slider from 'react-slick'
 import '../../../../../../node_modules/slick-carousel/slick/slick.css'
 import '../../../../../../node_modules/slick-carousel/slick/slick-theme.css'
