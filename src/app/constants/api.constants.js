ed5b5460c5394f0ac338aaad0093d03f431f5588
Rifat-MMBD <rifat@momagicbd.com>
2022-10-17 13:40:06 +0600

    changed base url to prod

--- src/app/constants/api.constants.js
+++ src/app/constants/api.constants.js
@@ -1,7 +1,7 @@
-// base_url_prod = 'http://192.168.33.32:8080'
+// base_url_prod = 'http://192.168.33.76:8085'
 // base_url_local = 'http://172.16.34.108:8083'
 // base_url_real = 'http://103.23.31.34:8080'
-export const BASE_URL='http://103.23.31.34:8080'
+export const BASE_URL='http://192.168.33.76:8085'
 export const GET_ORGANIZATION_LIST = `${BASE_URL}/partnerapi/organization/search`
 export const GET_ROLE_LIST = `${BASE_URL}/partnerapi/role/search`
 export const UPLOAD_IMAGE_BASE64 = `${BASE_URL}/partnerapi/image/upload`
