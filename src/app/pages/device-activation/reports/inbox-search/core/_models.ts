1f534b68d1cba838db85d221b2a0db97b030238e
Rifat-MMBD <rifat@momagicbd.com>
2022-11-06 16:31:08 +0600

    implemented inbox search

---
+++ src/app/pages/device-activation/reports/inbox-search/core/_models.ts
@@ -0,0 +1,43 @@
+import {ID, Response} from '../../../../../../_metronic/helpers'
+export type User = {
+  id?: ID
+  name?: string
+  mobile?: string
+  email?: string
+  address?: string
+  image?: string
+  roleList?: []
+  organization?: any
+  createdAt?: string
+  last_login?: string
+  two_steps?: boolean
+  joined_day?: string
+  online?: boolean
+  role?: any
+  position?: string
+  avatar?: string
+  initials?: {
+    label: string
+    state: string
+  }
+  password?: string
+  confirmPass?: string
+  brand?: any
+  firstImei?: any
+  firstImsi?: any
+  keyword?: any
+  operator?: any
+  orgMo?: any
+  vmsisdn?: any
+
+}
+
+export type UsersQueryResponse = Response<Array<User>>
+
+export const initialUser: User = {
+  image: 'avatars/300-6.jpg',
+  position: 'Art Director',
+  role: 'Administrator',
+  name: '',
+  email: '',
+}
