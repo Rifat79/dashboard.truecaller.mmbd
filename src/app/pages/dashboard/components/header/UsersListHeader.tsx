d08313eee592386f15c45acf8961ed66ebec7917
Rifat79 <ibarahim.rifat.127700@gmail.com>
2024-04-08 12:28:37 +0600

    added condition in toolbar in dashbaord

--- src/app/pages/dashboard/components/header/UsersListHeader.tsx
+++ src/app/pages/dashboard/components/header/UsersListHeader.tsx
@@ -1,21 +1,16 @@
+import {Can} from '../../../../../_metronic/redux/ability'
 import {useListView} from '../../core/ListViewProvider'
 import {UsersListToolbar} from './UserListToolbar'
 import {UsersListGrouping} from './UsersListGrouping'
-import {UsersListSearchComponent} from './UsersListSearchComponent'
 
 const UsersListHeader = () => {
   const {selected} = useListView()
   return (
-    <>
-      {/* <UsersListSearchComponent /> */}
-      {/* begin::Card toolbar */}
+    <Can access='Truecaller Summary' group='/dashboard'>
       <div className='card-toolbar'>
-        {/* begin::Group actions */}
         {selected.length > 0 ? <UsersListGrouping /> : <UsersListToolbar />}
-        {/* end::Group actions */}
       </div>
-      {/* end::Card toolbar */}
-    </>
+    </Can>
   )
 }
 
